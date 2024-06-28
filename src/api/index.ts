315dc68e9c48a1a66fc99c5d76e131ff50204a86
mizphses <fuminori@mizphs.es>
2024-06-29 00:50:13 +0900

    remove get all tanzaku

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,22 +5,6 @@ const client = createClient<paths>({
   baseUrl: process.env.NEXT_PUBLIC_ENDPOINT,
 })
 
-export const getAllTanzaku = async () => {
-  const res = await client.GET('/tanzaku/{projectId}/list', {
-    params: {
-      path: {
-        projectId: process.env.NEXT_PUBLIC_EVENTID || '',
-      },
-    },
-  })
-
-  if (res.error) {
-    throw new Error(res.error)
-  }
-
-  return res.data
-}
-
 export const getTenTanzaku = async () => {
   const res = await client.GET('/tanzaku/{id}', {
     params: {
